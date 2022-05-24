6b46d3fbcad1af9a89c51743c8aa1e94bdae88b2
joharris18 <johnisabelo18@gmail.com>
2022-05-24 13:28:52 +0800

    app relaunch

--- New folder/co3584e3d88401f28db02c6a3/App.js
+++ New folder/co3584e3d88401f28db02c6a3/App.js
@@ -4,15 +4,19 @@ import {Switch, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
-import "./styles.css"
 
 function App() {    
     return (
         <div>
             <Header />
             <Switch>
-                <Route path="/" exact component={Photos} />
-                <Route path="/cart" exact component={Cart} />
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+                
+                <Route path="/cart">
+                    <Cart />
+                </Route>
             </Switch>
         </div>
     )
--- New folder/co3584e3d88401f28db02c6a3/index.js
+++ New folder/co3584e3d88401f28db02c6a3/index.js
@@ -1,18 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {createRoot} from "react-dom/client"
 import {BrowserRouter as Router} from "react-router-dom"
 
 import {ContextProvider} from "./Context"
 import App from "./App"
 
-const container = document.getElementById("root")
-const root = createRoot(container)
-
-root.render(
+ReactDOM.render(
     <ContextProvider>
         <Router>
             <App />
         </Router>
-    </ContextProvider>
+    </ContextProvider>, 
+    document.getElementById("root")
 )
